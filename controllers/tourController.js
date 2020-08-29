ce949f3267e0812509beec9061974a46682876de
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-29 14:26:49 +0200

    apply delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -75,9 +75,18 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
