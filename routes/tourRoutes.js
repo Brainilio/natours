b75a2b8a248322e8f11ff48685c8d2d502c660a1
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-29 13:17:28 +0200

    connect mongoose to mongodb

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,23 +1,24 @@
-const express = require('express')
-const tourController = require('./../controllers/tourController');
-const router = express.Router()
+const express = require('express');
+const tourController = require('../controllers/tourController');
 
+const router = express.Router();
 
-// ------- MIDDLEWARE  ------ // 
+// ------- MIDDLEWARE  ------ //
 
 //checks id on each :id request
-router.param('id', tourController.checkID)
+router.param('id', tourController.checkID);
 
+// ----------- HTTP ROUTES -------------- //
 
-// ----------- HTTP ROUTES -------------- // 
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
 
-router.route('/')
-     .get(tourController.getAllTours)
-     .post(tourController.checkBody, tourController.createTour)
-
-router.route('/:id')
-     .get(tourController.getTour)
-     .patch(tourController.updateTour)
-     .delete(tourController.deleteTour)
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({
   path: './config.env',
 });
+
+const DB = process.env.DATABASE;
+// DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+//connect mongoose
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`Running mongoose...`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = require('./app');
 
