6a163feccc303c5cbd358a84cce84ece45d1046c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-29 14:22:24 +0200

    patch method done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,8 +24,7 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    const id = await +req.params.id;
-    const tour = await Tour.findById(id);
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: tour,
@@ -38,13 +37,25 @@ exports.getTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'updated tour',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
