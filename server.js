66ad615ef2c3c826495cbf6669dc15a19c3141bc
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-29 10:20:08 +0200

    dotenv

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('./app');
+const dotenv = require("dotenv")
+dotenv.config({
+	path: "./config.env",
+})
+const app = require("./app")
 
 // -------------------- LISTENING TO SERVER -------- //
-const port = 3000;
+const port = process.env.PORT || 8000
 app.listen(port, () => {
-     console.log(`App running on port ${port}`)
+	console.log(`App running on port ${port}`)
 })
