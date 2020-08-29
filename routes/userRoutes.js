4f56cb1153f0d23a5dcdbaf8f40c695cf42ff3fa
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-29 08:32:42 +0200

    fixed bug userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.route('/')
      .get(userController.getAllUsers)
      .post(userController.createUser)
 
-router.route('/')
+router.route('/:id')
      .get(userController.getUser)
      .patch(userController.updateUser)
      .delete(userController.deleteUser)
