366938f07708b7c55da3a172eb38538ded9d60db
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-31 20:39:03 +0200

    operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,10 +10,24 @@ const sendErrorDev = (err, res) => {
 
 //production error
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    messsage: err.message,
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      messsage: err.message,
+    });
+  }
+  // Programming or other unknown error: don't leak error details.
+  else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong.',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
