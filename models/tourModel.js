2f9562fff0faa5329a2de17a6195eb27ec707103
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-31 20:21:30 +0200

    woo

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,105 +1,105 @@
-const mongoose = require("mongoose")
-const slugify = require("slugify")
-const validator = require("validator")
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 //schema to enforce rules for model
 const tourSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			require: [true, "A city must have a name"],
-			unique: true,
-			maxlength: [40, "City must have less or equal then 40 characters"],
-			minlength: [5, "City must have more than 10 characters"],
-			validate: [validator.isAlpha, "City name must contain characters only!"],
-		},
-		duration: {
-			type: Number,
-			required: [true, "A tour must have a duration!"],
-		},
-		groupSize: {
-			type: Number,
-			required: [true, "A tour must have a group size"],
-		},
-		difficulty: {
-			type: String,
-			required: [true, "A tour must have a difficulty"],
-			enum: {
-				values: ["easy", "medium", "difficult"],
-				message: "Difficulty is either: easy, medium or difficult",
-			},
-		},
-		ratingsAverage: {
-			type: Number,
-			default: 2.5,
-			min: [1, "Rating must be above 1.0"],
-			max: [5, "Rating must be below 5.0"],
-		},
-		ratingsQuantity: {
-			type: Number,
-			default: 0,
-		},
-		price: {
-			type: Number,
-			required: [true, "A tour must have a price."],
-		},
-		priceDiscount: {
-			type: Number,
-			validate: {
-				validator: function (val) {
-					// This only points to current doc on new document creation.
-					return val < this.price //100 < 200; price discount should be lower
-				},
-				message: "Discount price ({VALUE}) should be below regular price!",
-			},
-		},
-		summary: {
-			type: String,
-			trim: true,
-			required: [true, "A tour must have a description"],
-		},
-		description: {
-			type: String,
-			trim: true,
-		},
-		imageCover: {
-			type: String,
-			required: [true, "Add at least one picture!"],
-		},
-		images: [String],
-		createdAt: {
-			type: Date,
-			default: Date.now(),
-		},
-		startDates: [Date],
-		slug: String,
-	},
-	{
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true },
-	}
-)
+  {
+    name: {
+      type: String,
+      require: [true, 'A city must have a name'],
+      unique: true,
+      maxlength: [40, 'City must have less or equal then 40 characters'],
+      minlength: [5, 'City must have more than 10 characters'],
+      validate: [validator.isAlpha, 'City name must contain characters only!'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration!'],
+    },
+    groupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 2.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price.'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // This only points to current doc on new document creation.
+          return val < this.price; //100 < 200; price discount should be lower
+        },
+        message: 'Discount price ({VALUE}) should be below regular price!',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Add at least one picture!'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+    slug: String,
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 // ---------- VIRTUAL METHODS --------- //
-tourSchema.virtual("durationWeeks").get(function () {
-	return this.duration / 7
-})
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
 
 // -------- DOC MIDDLEWARE: RUNS BEFORE .SAVE() AND .CREATE() //
-tourSchema.pre("save", function (next) {
-	this.slug = slugify(this.name, { lower: true })
-	next()
-})
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 // ------- QUERY MIDDLEWARE: RUNS BEFORE QUERY CALLS -- //
 // as soon as you hit this route, you can chain a method in between it
-tourSchema.pre("find", function (next) {
-	next()
-})
+tourSchema.pre('find', function (next) {
+  next();
+});
 
 // ------ AGGREGATION MIDDLEWARE -- //
 //none
 
-const Tour = mongoose.model("Tour", tourSchema)
+const Tour = mongoose.model('Tour', tourSchema);
 
-module.exports = Tour
+module.exports = Tour;
