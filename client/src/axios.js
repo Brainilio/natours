596dd881189ec689b93160368d401dc29d0cfef0
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-10-25 17:01:55 +0100

    new server

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-	baseURL: "http://159.89.101.212:8000/api/v1/",
+	baseURL:
+		"http://ec2-18-192-13-45.eu-central-1.compute.amazonaws.com:8000/api/v1/",
 	headers: {
 		Authorization: localStorage.getItem("token"),
 	},
