39c796da8737ada9c0b56510318cf09618b87bae
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-12-02 13:37:29 +0100

    new build

--- client/src/pages/Home/Landing/Landing.js
+++ client/src/pages/Home/Landing/Landing.js
@@ -4,7 +4,11 @@ import Background from "../../../resource/backgroundimg.jpg"
 const Landing = () => {
 	return (
 		<section className="home-landing">
-			<img src={Background} alt="backgroundimage" />
+			<img
+				src={Background}
+				className="home-landing-picture"
+				alt="backgroundimage"
+			/>
 			<div className="home-landing-text">
 				<span className="home-landing-title">NATOURS</span>
 				<span className="home-landing-motto">Tour Agency</span>
